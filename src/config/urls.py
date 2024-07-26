3266493e1589bd0004366cbb777c2b3ab484cbb9
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-07-27 00:56:43 +0300

    fixed: urls.py

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from mail_recipient.views import email_list
+from users.views import add_email_account
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", add_email_account, name="add_email_account"),
+    path("mail_recipient/", email_list, name="email_list")
 ]
