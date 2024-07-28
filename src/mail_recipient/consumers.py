d332dc5018125151385e745219f97cd91df8bd8f
Vladimir Vasiliev <vladvasiliev52@gmail.com>
2024-07-28 17:44:50 +0300

    fixed: consumers.py, email_list.html

--- src/mail_recipient/consumers.py
+++ src/mail_recipient/consumers.py
@@ -1,35 +1,44 @@
 import json
+from typing import Any, Coroutine
+
 from channels.generic.websocket import AsyncWebsocketConsumer
+
 from core.utils import fetch_emails
 from email_account.models import EmailAccount
 
 
 class EmailListConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
+    async def connect(self) -> Coroutine[Any, Any, None]:
+        return await self.accept()
 
-    async def disconnect(self, close_code):
+    async def disconnect(self, close_code: Any) -> Coroutine[Any, Any, None]:
         pass
 
-    async def receive(self, text_data):
+    async def receive(self,
+                      text_data: Any = None,
+                      bytes_data: Any = None) -> Coroutine[Any, Any, None]:
         text_data_json = json.loads(text_data)
-        if text_data_json.get("action") == "fetch_emails":
-            email = text_data_json.get('email')
-            if email:
-                email_account = await EmailAccount.objects.filter(email=email).afirst()
-                if email_account:
-                    emails = await fetch_emails(email_account)
-                    await self.send(text_data=json.dumps({
-                        'type': 'email_list',
-                        'emails': emails
-                    }))
-                else:
-                    await self.send(text_data=json.dumps({
-                        'type': 'error',
-                        'message': 'Email account not found'
-                    }))
-            else:
-                await self.send(text_data=json.dumps({
-                    'type': 'error',
-                    'message': 'Email is required'
-                }))
+        action = text_data_json.get("action")
+        if action != "fetch_emails":
+            return await self.send(text_data=json.dumps({
+                'type': 'error',
+                'message': f'Неподдерживаемое действие: {action}'
+            }))
+        email = text_data_json.get('email')
+        if not email:
+            return await self.send(text_data=json.dumps({
+                'type': 'error',
+                'message': 'Требуется электронная почта'
+            }))
+        email_account = await EmailAccount.objects.filter(email=email).afirst()
+        if email_account:
+            emails = await fetch_emails(email_account)
+            return await self.send(text_data=json.dumps({
+                'type': 'email_list',
+                'emails': emails
+            }))
+        else:
+            return await self.send(text_data=json.dumps({
+                'type': 'error',
+                'message': 'Электронная почта не найдена'
+            }))
